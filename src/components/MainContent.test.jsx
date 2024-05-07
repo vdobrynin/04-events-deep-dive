5de68bea8eb3b4425845acabe01f5193fb26cf67
Vasiliy Dobrynin <vdobrynin@sbcglobal.net>
2024-05-06 20:48:01 -0700

    added new comment

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // My tests
+// My other tests
 
 describe('MainContent', () => {
   it('should render a button', () => {
